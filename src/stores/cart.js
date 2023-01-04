e674428b9920be748a78e42f0e65fca4ccff06e0
Gad Omuse <E145@ad.oxygenehq.com>
2023-01-04 10:52:17 +0300

    add to cart done

--- src/stores/cart.js
+++ src/stores/cart.js
@@ -1,23 +1,36 @@
 import { defineStore } from "pinia";    
 
 export const useCartStore = defineStore('cart', {
-
     state: () => {
         return { items: [] }
     },
     getters:{
-        totalCartItem: (state) => state.items.reduce((total, i) => total + i.quantity, 0)
+        totalCartItem: (state) => state.items.reduce((total, i) => total + i.quantity, 0),
+        subTotal : (state) => state.items.reduce((total, i) => total + i.item.price * i.quantity, 0),
+        taxTotal () {
+            return this.subTotal * 0.1;
+        },
+        total(){
+            return this.subTotal + this.taxTotal;
+        }
     },
     actions: {
         addToCart(item){
             // console.log(item)
             const exisingItem = this.items.find(i => i.id === item.id);
-            if(exisingItem){
-                exisingItem.quantity++;
+            console.log(exisingItem)
+            if(exisingItem === true){
+                exisingItem.item.quantity++;
+                return;
             }else{
                 this.items.push({item, quantity: 1});
                 console.log(this.items)
             }
+        },
+        removeItemFromCart (item) {
+            const index = this.items.findIndex(i => i.id === item.id);
+            this.items.splice(index, 1);
         }
+
     }
 })  
--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,52 +1,101 @@
 <script setup>
 import { useCartStore } from "@/stores/cart";
 const cartStore = useCartStore();
-console.log(cartStore.totalCartItem)
-
+console.log(cartStore.items);
 </script>
 
 <template>
   <main class="container">
-   <div class="row">
-     <div class="col-10 ms-auto" v-if="cartStore.totalCartItem">
-        <h4>Cart ( {{cartStore.totalCartItem}} )</h4>
-       <div class="card mt-4" v-for="product in cartStore.items" :key="product.id">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-6">
-                       <div class="prod-img">
-                        <img :src="product.item.image" /> 
-                       </div>
+    <div class="row" v-if="cartStore.totalCartItem">
+        <h4>Cart ( {{ cartStore.totalCartItem }} )</h4>
+
+      <div class="col-9">
+        <div class="table-responsive">
+          <table class="table table-hover">
+            <thead>
+              <tr>
+                <th></th>
+                <th></th>
+                <th>Product</th>
+                <th>Price</th>
+                <th>Items</th>
+                <th>Remove</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="({ item }, i) in cartStore.items" :key="item.id">
+                <td></td>
+                <td>
+                  <div class="prod-img">
+                    <img :src="item.image" />
+                  </div>
+                </td>
+                <td class="pt-2 text-start">{{ item.title }}</td>
+                <td>Ksh {{ item.price }}</td>
+                <td class="text-center">
+                  <div class="p-3">
+                    <div
+                      class="input-group w-auto justify-content-end align-items-center"
+                    >
+                      <input
+                        type="button"
+                        value="-"
+                        class="button-minus border icon-shape icon-sm mx-1"
+                      @click="removeQuantity"/>
+                      <input
+                        type="number"
+                        step="1"
+                        max="100"
+                        min="0"
+                        v-model="cartStore.items[i].quantity"
+                        name="quantity"
+                        class="quantity-field text-center w-25"
+                      />
+                      <input
+                        type="button"
+                        value="+"
+                        class="button-plus border icon-sm lh-0"
+                        @click="i"/>
                     </div>
-                    <div class="col-lg-6">
-                        <h3>Cart</h3>
-                        <hr>
-                        <h5>{{ product.item.title }}</h5>
-                        <h5>Ksh {{ product.item.price }}</h5>
-
-                    </div>
-                </div>
-            </div>
-       </div>
-       <div class="row mt-4" >
-            <div class="col-6 text-start">
-                <button class="btn btn-outline-primary">Remove</button>
-            </div>
-            <div class="col-6 text-text-end">
-                <button class="btn btn-outline-primary">Checkout</button>
-            </div>
+                  </div>
+                </td>
+                <td>
+                  <div class="col-5 text-end">
+                    <button class="btn btn-outline-danger" @click="cartStore.removeItemFromCart(item)"> x </button>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-     </div>
-        <div class="col-10 ms-auto text-center" v-else>
-            <h4>Cart is empty , Continue  
-                <router-link :to="{name: 'home'}" class="btn btn-outline-success">
-                    Shopping
-                 </router-link>
-            </h4>
+      </div>
+      <div class="col-3 pt-5">
+        <div class="card">
+          <div class="card-body">
+            <ul>
+              <ul>
+                <li>subTotal : Ksh {{ cartStore.subTotal }}</li>
+                <li>Tax : Ksh {{ cartStore.taxTotal }}</li>
+                <li>Total : Ksh {{ cartStore.total }}</li>
+              </ul>
+            </ul>
+            <button class="btn btn-info">Checkout</button>
+          </div>
         </div>
-
-   </div>
-
-  
+      </div>
+    </div>
+    <div class="row" v-else>
+      <div class="col-12">
+        <h4 class="text-center">Your cart is empty</h4>
+      </div>
+      <div class="col-12 ms-auto text-center">
+        <h4>
+          Cart is empty , Continue
+          <router-link :to="{ name: 'home' }" class="btn btn-outline-success">
+            Shopping
+          </router-link>
+        </h4>
+      </div>
+    </div>
   </main>
 </template>
